dd77f52dc2f46361521650a883bf229152050ca2
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-01 16:46:24 -0500

    finished the projects portion

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const actions = require('./data/helpers/actionModel.js');
-const mappers = require('./data/helpers/mappers.js');
 const projects = require('./data/helpers/projectModel.js');
 
 const express = require('express');
@@ -9,6 +8,8 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000' }));
 
+
+//Actions
 server.get('/api/actions', (req, res) => {
     actions
     .get()
@@ -80,4 +81,74 @@ actions
 })
 
 
+//projects
+server.get('/api/projects', (req, res) => {
+    projects
+    .get()
+    .then(projects => {
+        res.json({ projects })
+    })
+    .catch(error => {
+        res.status(500)
+        res.json({ message: "The projects information could not be retrieved."})
+    })
+}) 
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    if(req.params.id == undefined) {
+        res.status(404)
+        res.json({ message: "The projects with the specified ID does not exist." })
+    }
+    else {
+        projects
+    .get(id)
+    .then(projects => {
+        res.json({ projects })
+    })
+    .catch(error => {
+        res.status(500)
+        res.json({ message: "The projects information could not be retrieved."})
+    })
+}}) 
+server.post('/api/projects', (req, res) => {
+    const { description, name, completed } = req.body;
+    projects
+    .insert(req.body) 
+    .then(projects => {
+        res.json({ projects })
+    })
+    .catch(error => {
+        res.status(500)
+        res.json({ message: "The projects information could not be added."})
+    })
+  })
+server.put('/api/projects/:id', (req, res) => {
+    const { description, name, completed } = req.body;
+    const { id } = req.params;
+    if(!description || !name) {
+       res.status(400)
+       res.json({ message: "Please include a description." })
+    }
+    projects
+    .update(id, req.body)
+    .then(projects => {
+       res.json({ projects})
+    })
+    .catch(error => {
+        res.status(500)
+        res.json({ message: "The projects information could not be changed."})
+    })
+   })
+   server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects
+    .remove(id)
+    .then(projects => {
+        res.json({ projects })
+    }) 
+    .catch(error => {
+        res.status(500)
+        res.json({ message: "The projects information could not be deleted."})
+    }) 
+    })
 server.listen(port, () => console.log(`Server running on port ${port}`));
